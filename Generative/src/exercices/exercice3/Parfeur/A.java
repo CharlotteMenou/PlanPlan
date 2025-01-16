55921e00ae5055317b0f8bb5fe5ed5c038dc9a9e
Kuonagou <anouk.gouhier@gmail.com>
2025-01-16 10:29:53 +0100

    debut exo 3-4

---
+++ Generative/src/exercices/exercice3/Parfeur/A.java
@@ -0,0 +1,12 @@
+package exercices.exercice3.Parfeur;
+
+import java.util.ArrayList;
+
+public class A {
+    String id;
+    String value;
+    ArrayList<B> theBs;
+    public A() {
+        theBs = new ArrayList<B>();
+    }
+}
---
+++ Generative/src/exercices/exercice3/Parfeur/B.java
@@ -0,0 +1,6 @@
+package exercices.exercice3.Parfeur;
+
+public class B {
+    String name;
+    String val;
+}
